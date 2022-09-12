45056a010d9d1904ee8220d2348f3d6716e5c40a
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-13 01:01:35 +0200

    Add files via upload

---
+++ Eau.01.py
@@ -0,0 +1,17 @@
+#Créez un programme qui affiche toutes
+# les différentes combinaisons
+# de deux nombre entre 00 et 99 dans l’ordre croissant.
+
+list1 = [0,0]
+list2 = [0,0]
+
+while True :
+
+    if list1[1] < 9 :
+        list1[1] = list1[1] + 1
+        print(list2, list1)
+    if list1[1] == 9:
+        list1[0] = list1[0] + 1
+    if list1[0] < 10 :
+        print(list2, list1)
+
