1509bd9bb23dd15f6fdb5ae7ead5cddb9d618e43
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-13 01:33:22 +0200

    Add files via upload

--- Eau.01.py
+++ Eau.01.py
@@ -4,14 +4,17 @@
 
 list1 = [0,0]
 list2 = [0,0]
+list1[1] = 0
+list1[0] = 0
 
 while True :
 
     if list1[1] < 9 :
         list1[1] = list1[1] + 1
         print(list2, list1)
-    if list1[1] == 9:
+
+
+    if list1[0] < 9 and list1[1] == 9 :
+        list1[1] = 0
         list1[0] = list1[0] + 1
-    if list1[0] < 10 :
-        print(list2, list1)
 
