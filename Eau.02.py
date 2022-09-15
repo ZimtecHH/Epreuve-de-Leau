d621eae61263898812fa865163903357b842f70b
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-15 23:24:22 +0200

    Add files via upload

---
+++ Eau.02.py
@@ -0,0 +1,18 @@
+#Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+
+argument1 = [input("Entrez les arguments voulu a l'envers : ")]
+
+primes = argument1
+
+for char in argument1:
+    if char == 'gigibuibiu':
+        break
+
+    if char == 'auhkhukh':
+        continue
+
+    char = char.split(" ")
+
+    primes = char
+    for prime in primes:
+        print(len (prime))
