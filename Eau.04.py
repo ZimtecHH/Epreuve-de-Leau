b4429a4736044e5b7e12fa9af87eb8385b58e13b
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-19 21:24:21 +0200

    Add files via upload

---
+++ Eau.04.py
@@ -0,0 +1,21 @@
+#Créez un programme qui affiche le premier
+#nombre premier supérieur au nombre donné en argument
+
+liste = [0]
+# 1 not being a prime number, is ignored
+while True :
+    if liste[0] < 100 :
+        liste[0] = liste[0] + 1
+        print(liste)
+        k = 13
+
+        if liste[0] > 1:
+            for i in range(2, int(liste[0] / 2) + 1):
+                if (liste[0] % i) == 0:
+                    print("It is not a prime number")
+                    break
+            else:
+                print("It is a prime number")
+
+        else:
+            print("It is not a prime number")
