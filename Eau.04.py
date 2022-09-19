93de0639745ac5adccd95cdcc19deda4ae11a398
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-19 21:45:08 +0200

    Add files via upload

--- Eau.04.py
+++ Eau.04.py
@@ -7,16 +7,13 @@ liste[0] = int(input("Rentrer un chiffre : "))
 while True :
     if liste[0] < 100 :
         liste[0] = liste[0] + 1
-        print(liste)
-        k = 13
+
 
         if liste[0] > 1:
             for i in range(2, int(liste[0] / 2) + 1):
                 if (liste[0] % i) == 0:
-                    print("It is not a prime number")
                     break
             else:
+                print(liste)
                 print("It is a prime number")
-
-        else:
-            print("It is not a prime number")
+                break
