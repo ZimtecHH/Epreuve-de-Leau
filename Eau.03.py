907d99976f4a922b4c510fd25ace6ac632c26c39
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-19 20:57:17 +0200

    Add files via upload
    a finir

---
+++ Eau.03.py
@@ -0,0 +1,16 @@
+#Suite de Fibonacci le n ème élement
+
+def Fibonacci(a,b):
+    a + b
+    return
+
+#SUITE MAGIQUE (0, 1, 1, 2)
+
+
+list[0,1,1,2]
+
+print(list)
+
+# x = [1,2,3]
+# y = x[:]
+# y[0] = 9
