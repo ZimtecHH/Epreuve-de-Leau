463839cbb6d641eb43f71d0d2b9a0ce1dd744138
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-21 01:16:19 +0200

    Add files via upload

--- Eau.05.py
+++ Eau.05.py
@@ -3,8 +3,15 @@
 
 premiere_chaine = input("Entrez caractère : ")
 
+if( premiere_chaine.isdigit()):
+    print("Error")
+    pass
+
 deuxieme_chaine = input("entrez 4 chaine de caractère : ")
 
+if( deuxieme_chaine.isdigit()):
+    print("Error")
+    pass
 
 if deuxieme_chaine.startswith(premiere_chaine[:4]):
     print(True)
