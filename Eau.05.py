1e728d59383683bd2e9ba1c66eee35cc63469a91
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-21 00:52:22 +0200

    Add files via upload

---
+++ Eau.05.py
@@ -0,0 +1,14 @@
+#Créez un programme qui détermine
+# si une chaîne de caractère se trouve dans une autre.
+
+premiere_chaine = input("Entrez caractère : ")
+deuxieme_chaine = input("Deuxième chaine de caractère : ")
+
+
+if deuxieme_chaine.startswith(premiere_chaine[:4]):
+    print(True)
+elif deuxieme_chaine.endswith(premiere_chaine[-4:]):
+    print(True)
+
+else:
+    print(False)
