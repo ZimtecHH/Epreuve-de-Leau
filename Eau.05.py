1f6f03a31e3ea137cee2bdeda4c43ac934881658
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-21 01:04:00 +0200

    Add files via upload

--- Eau.05.py
+++ Eau.05.py
@@ -2,10 +2,18 @@
 # si une chaîne de caractère se trouve dans une autre.
 
 premiere_chaine = input("Entrez caractère : ")
+
+if premiere_chaine == "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0" :
+    print("Error")
+
+
 deuxieme_chaine = input("Deuxième chaine de caractère : ")
 
+if deuxieme_chaine == "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0" :
+    print("Error")
 
-if deuxieme_chaine.startswith(premiere_chaine[:4]):
+
+elif deuxieme_chaine.startswith(premiere_chaine[:4]):
     print(True)
 elif deuxieme_chaine.endswith(premiere_chaine[-4:]):
     print(True)
