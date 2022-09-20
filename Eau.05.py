6eec34a52aded4a762b56fa4f7bd1c05ca901fea
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-21 01:08:33 +0200

    Add files via upload

--- Eau.05.py
+++ Eau.05.py
@@ -3,16 +3,12 @@
 
 premiere_chaine = input("Entrez caractère : ")
 
-deuxieme_chaine = input("Deuxième chaine de caractère : ")
+deuxieme_chaine = input("entrez 4 chaine de caractère : ")
 
-if deuxieme_chaine == "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0" :
-    print("Error")
 
-elif premiere_chaine == "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0" :
-    print("Error")
-
-elif deuxieme_chaine.startswith(premiere_chaine[:4]):
+if deuxieme_chaine.startswith(premiere_chaine[:4]):
     print(True)
+
 elif deuxieme_chaine.endswith(premiere_chaine[-4:]):
     print(True)
 
